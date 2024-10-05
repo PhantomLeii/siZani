fac61b56719339d85f3e2c17aab3fc2b8ff6a1cb
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-10-05 20:13:19 +0200

    Update: add basic navbar

---
+++ components/NavbarMenu.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+
+export default function NavbarMenu() {
+  return (
+    <Navbar className="w-full fixed top-0 left-0 py-6">
+      <Navbar.Brand href="/">
+        <span className="self-center whitespace-nowrap text-3xl tracking-wide font-extrabold">
+          siZani
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
